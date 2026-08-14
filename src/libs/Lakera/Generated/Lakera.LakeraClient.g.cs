28f6976286003810aafecba4c496d678e6596683
Konstantin S. <havendv@gmail.com>
2026-08-14 19:50:59 +0400

    feat: Updated OpenAPI spec (#35)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Lakera/Generated/Lakera.LakeraClient.g.cs
+++ src/libs/Lakera/Generated/Lakera.LakeraClient.g.cs
@@ -36,10 +36,17 @@ namespace Lakera
 
         /// <inheritdoc/>
         public global::Lakera.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Lakera.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Lakera.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
